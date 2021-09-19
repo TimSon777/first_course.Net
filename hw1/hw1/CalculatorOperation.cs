4aea87816dfbb9a22b69d4914bc027b18bb73beb
TimSon777 <dusalimov.tima@mail.ru>
2021-09-19 20:24:46 +0300

    Add hw1 and CodecovReport.xml

---
+++ hw1/hw1/CalculatorOperation.cs
@@ -0,0 +1,10 @@
+﻿namespace hw1
+{
+    public enum CalculatorOperation : byte
+    {
+        Plus = 1,
+        Minus = 2,
+        Multiply = 3,
+        Divide = 4
+    }
+}
---
+++ hw1/hw1/ErrorCode.cs
@@ -0,0 +1,10 @@
+﻿namespace hw1
+{
+    public enum ErrorCode
+    {
+        Correct = 0,
+        WrongArgLength = 1,
+        WrongArgFormatInt = 2,
+        WrongArgFormatOperation = 3,
+    }
+}
