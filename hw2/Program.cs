5d6b6a8f5a9b2f8f6e07ef998b4febc18a3f6d80
TimSon777 <dusalimov.tima@mail.ru>
2021-09-22 14:11:55 +0300

    add hw2

---
+++ hw2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using hw2_il;
+
+namespace hw2
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            var check = Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
+            var isError = ErrorMessages.IsErrorCodeDisplayErrorMessage(check, args);
+            if (isError) return (int) check;
+            
+            var result = Calculator.Calculate(val1, operation, val2);
+            Console.WriteLine($"Result is: {result}");
+            return (int) ErrorCode.Correct;
+        }
+    }
+}
