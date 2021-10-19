5e48ca8fcd67276c225267b115342ad84a4407e6
TimSon777 <dusalimov.tima@mail.ru>
2021-10-19 18:36:52 +0300

    add additional task MaybeBuilder

---
+++ addTask1/addTask1_csharp/MaybeBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.FSharp.Core;
+
+namespace addTask1_csharp
+{
+    public static class MaybeBuilder
+    {
+        public static FSharpOption<TZero> Zero<TZero>() => null;
+
+        public static FSharpOption<TFunc> Bind<TElement, TFunc>(FSharpOption<TElement> x,
+            FSharpFunc<TElement, FSharpOption<TFunc>> f) => x == null ? null : f.Invoke(x.Value);
+        
+        public static FSharpOption<TReturn> Return<TReturn>(TReturn x) => FSharpOption<TReturn>.Some(x);
+    }
+}
