cc4dec9d366aec7eae18a1092035574887ce8d45
TimSon777 <dusalimov.tima@mail.ru>
2021-11-02 01:04:16 +0300

    удалил лишнее

--- hw5/Expression.fs
+++
@@ -1,8 +0,0 @@
-﻿module hw5.Expression
-
-[<CLIMutable>]
-type Expression = {
-    V1:decimal
-    Operation:Operation 
-    V2:decimal
-}
--- hw5/ResultBuilder.fs
+++
@@ -1,9 +0,0 @@
-﻿module hw5.ResultBuilder
-    
-type ResultBuilder() =
-    member b.Bind(x, f) =
-        match x with
-        | Ok ok -> f ok
-        | Error error  -> Error error
-    member b.Return x = Ok x
-let result = ResultBuilder()
