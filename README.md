9f76e7ab7c9bd8052b916b2f57c66b3111b57e4e
TimSon777 <dusalimov.tima@mail.ru>
2021-11-08 21:01:40 +0300

    э

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # first_course.Net
+[![codecov](https://codecov.io/gh/TimSon777/first_course.Net/branch/2k-181/graph/badge.svg?token=HPK2TNIJ2X)](https://codecov.io/gh/TimSon777/first_course.Net)
---
+++ hw7/Interfaces/ICalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace hw7.Interfaces
+{
+    public interface ICalculator
+    {
+        string Add(double firstValue, double secondValue);
+        string Subtract(double firstValue, double secondValue);
+        string Multiply(double firstValue, double secondValue);
+        string Divide(double firstValue, double secondValue);
+    }
+}
