04a74e92ae9c71f73088aac8adfb88755696e727
TimSon777 <dusalimov.tima@mail.ru>
2021-11-22 17:33:48 +0300

    Update Calculator view and controller by interface

---
+++ hw8/Controllers/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using hw8.Models;
+using hw8.Services.CalculatorLogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hw8.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Calculate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate([FromServices] IExpressionCalculator calculator, string expression)
+        {
+            var f = calculator.GetFuncByExpression(expression);
+            
+            var model = new CalculatorModel
+            {
+                Answer = f()
+            };
+            
+            return View(model);
+        }
+    }
+}
