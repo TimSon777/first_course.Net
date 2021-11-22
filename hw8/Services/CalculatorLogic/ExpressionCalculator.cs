25a05e1ca0875a2f1f2f786cd662aa1f00f42786
TimSon777 <dusalimov.tima@mail.ru>
2021-11-22 17:32:16 +0300

    Add interface and class for get expression calculator

---
+++ hw8/Services/CalculatorLogic/ExpressionCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace hw8.Services.CalculatorLogic
+{
+    public class ExpressionCalculator : IExpressionCalculator
+    {
+        public Expression<Func<string>> ParseStringIntoExpression(string str)
+        {
+            Expression<Func<string>> e = () => "777";
+            return e;
+        }
+    }
+}
---
+++ hw8/Services/CalculatorLogic/IExpressionCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace hw8.Services.CalculatorLogic
+{
+    public interface IExpressionCalculator
+    {
+        Expression<Func<string>> ParseStringIntoExpression(string str);
+        public Func<string> GetFuncByExpression(string str) => ParseStringIntoExpression(str).Compile();
+    }
+}
