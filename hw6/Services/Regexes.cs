91933813d090c682839740b6b66fc25c8827f63d
TimSon777 <dusalimov.tima@mail.ru>
2021-12-10 17:49:14 +0300

    Delete hw6

--- hw6/Services/Regexes.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.RegularExpressions;
-
-namespace hw6.Services
-{
-    public static class Regexes
-    {
-        public static readonly Regex RegexCamelCase = new(@"([A-Z]+(?![a-z])|[A-Z][a-z]+|[0-9]+|[a-z]+)");
-    }
-}
